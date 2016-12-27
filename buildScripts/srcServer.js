7dcd58563e473b4ca49fba4a25118747493955ad
Fan Yang <fan.yang.turner@live.co.uk>
2016-12-27 10:48:39 +0000

    Add lint, but not working...

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -8,6 +8,7 @@ import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev'
 
+/* eslint-disable no-console*/
 //var port = 3000
 const port = 3000;
 //var app = express();
